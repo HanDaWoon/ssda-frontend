487fffac1e1cf55b31dedd2df185e85ffaddb05b
HanDaWoon <bookpia.han@gmail.com>
2023-06-16 11:53:42 +0900

    수정

--- app/board/post/[id]/page.tsx
+++ app/board/post/[id]/page.tsx
@@ -61,7 +61,7 @@ export default function Post() {
       },
       body: JSON.stringify({
         fontOwner: "MSY",
-        fontName: "MSY",
+        fontName: post?.fontName,
         text: text,
       }),
     })
